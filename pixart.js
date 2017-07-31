25c68217a7ad357115ca05c01539fc9384745094
Saloni <saloni.1911@gmail.com>
2017-07-31 15:57:50 +1000

    commit_3

--- pixart.js
+++ pixart.js
@@ -2,10 +2,20 @@ var $brush = $('.brush');
 var $input = $('#color-field');
 var $button = $('#set-color');
 
+
 $button.on("click", function(event) {
 	$brush.css("background", $input[0].value);
+	// $brush.css("background", $input.val());
 	// console.log($input[0].value);
-
 	event.preventDefault();
 
 })
+
+var $body = $('body');
+// console.log($body[0]);
+var $newDiv = $('<div>').addClass('square')
+
+for(var i = 0; i < 20; i++) {
+	var $newDiv = $('<div>').addClass('square');
+	$body.append($newDiv);
+}
